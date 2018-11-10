cf6abcbbf2ec8e6b0340302bfa515169787cfbb8
byronluk <byronluk@gmail.com>
2018-11-09 18:33:21 -0800

    changing base webpack config

--- archetype/config/webpack/base.js
+++ archetype/config/webpack/base.js
@@ -1,10 +1,12 @@
 const webpack = require("webpack");
+const dotenv = require("dotenv-webpack");
 
 const config = {
   plugins: [
     new webpack.DefinePlugin({
       "process.env.HELLO": JSON.stringify("hello")
-    })
+    }),
+    new dotenv()
   ]
 };
 
